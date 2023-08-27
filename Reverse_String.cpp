de060d94c74fb2aa67a22520726df3a8ba7538a9
Vaani <vaanipathariya@gmail.com>
2023-08-27 14:10:50 +0530

    https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/

---
+++ Reverse_String.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the length of the array";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n/2;i++){
+        int temp=arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=temp;
+    }
+    for(int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    return 0;
+}
