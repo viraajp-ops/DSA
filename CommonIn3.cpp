9c7caf35d0d5371b5a6559dfeaef78f0bc4009b9
Vaani <vaanipathariya@gmail.com>
2023-08-30 20:00:23 +0530

    https://practice.geeksforgeeks.org/problems/common-elements1132/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article

---
+++ CommonIn3.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> commonElements(int a[],int b[],int c[],int n1,int n2,int n3){
+    vector<int >ans;
+    int ptr1=0,ptr2=0,ptr3=0;
+    while (ptr1<n1 && ptr2<n2 && ptr3<n3)
+    {
+        if (a[ptr1]==b[ptr2] && b[ptr2]==c[ptr3]){
+            if (ans[ans.size()-1]!=a[ptr1])
+            ans.push_back(a[ptr1]);
+            ptr1++;
+            ptr2++;
+            ptr3++;
+        }
+        else {
+            int mini=min(min(a[ptr1],b[ptr2]),c[ptr3]);
+            if (a[ptr1]==mini)
+            {
+                ptr1++;
+            }
+            else if (b[ptr2]==mini){
+                ptr2++;
+            }
+            else if (c[ptr3]==mini)
+            ptr3++;
+        }
+    }
+    return ans;
+}
+int main(){
+    int n1,n2,n3;
+    cin>>n1>>n2>>n3;
+    int a[n1],b[n2],c[n3];
+    for(int i=0;i<n1;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n2;i++){
+        cin>>b[i];
+    }
+    for(int i=0;i<n3;i++){
+        cin>>c[i];
+    }
+    vector<int>ans=commonElements(a,b,c,n1,n2,n3);
+    for(int i=0;i<ans.size();i++)
+    cout<<ans[i]<<" ";
+    return 0;
+}
