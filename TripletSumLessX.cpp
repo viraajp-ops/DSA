6504ee1b5fc4508ff713758b597aed98c5a42a51
Vaani <vaanipathariya@gmail.com>
2023-08-31 20:38:36 +0530

    slight changes

--- TripletSumLessX.cpp
+++ TripletSumLessX.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 long long int countTriplets(long long arr[], int n, long long sum){
     int count=0;
+    sort (arr,arr+n);
     for(int i=0;i<n-2;i++){
         int left=i+1;
         int right=n-1;
