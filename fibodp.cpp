b2d3f43f3044449045926358aeb7a2f83eeb0b11
Vaani <vaanipathariya@gmail.com>
2023-10-01 15:00:54 +0530

    Learning DP

---
+++ fibodp.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int fibo(int n){
+    int arr[n+1];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<n+1;i++){
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+    return arr[n];
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fibo(n-1);
+    return 0;
+}
