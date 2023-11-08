ad86d04defa0392d219c2de4488a4b154f77b762
Vaani <vaanipathariya@gmail.com>
2023-11-08 21:40:52 +0530

    https://leetcode.com/problems/longest-palindromic-substring/

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,9 +1,4 @@
-// This question uses Manachers algorithm 
-#include<bits/stdc++.h>
-using namespace std;
-string longestPalindrome(string s){
-    // Preprocessing stage :
-    char t[2*s.size()+1];
+char t[2*s.size()+1];
     int index=0;
     for(int i=0;i<2*s.size()+1;i++){
         if (i%2==0)
@@ -16,7 +11,6 @@ string longestPalindrome(string s){
     int l=1,r=1;
     // Starting from 1 since on 0th index we know their is no way possible for it to be same 
     for(int i=1;i<2*s.size()+1;i++){
-        if (l+r-i<2*s.size()+1)
         p[i]=max(0,min(r-i,p[l+r-i]));
         while (i+p[i]<2*s.size()+1 && i-p[i]>=0 && t[i+p[i]]==t[i-p[i]])
         {
@@ -36,10 +30,3 @@ string longestPalindrome(string s){
         }
     }
     return s.substr(place/2-(maxPos-1)/2,maxPos-1);
-}
-int main(){
-    string s;
-    cin>>s;
-    cout<<longestPalindrome(s);
-    return 0;
-}
