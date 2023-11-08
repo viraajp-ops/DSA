1e1355466f939fef4f67cd09123ddc9e924eb4ea
Vaani <vaanipathariya@gmail.com>
2023-11-08 17:42:17 +0530

    This question uses Manachers algorithm

---
+++ LongestPalindromicSubstring.cpp
@@ -0,0 +1,17 @@
+// This question uses Manachers algorithm 
+#include<bits/stdc++.h>
+using namespace std;
+string longestPalindrome(string s){
+    if (s.size()%2==0){
+        // Even length palindrome case
+    }
+    else {
+        // Odd length palindrome case 
+    }
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<longestPalindrome(s);
+    return 0;
+}
