b649949383b8f3cbe9aedbbc2759b391a0ea9280
Vaani <vaanipathariya@gmail.com>
2023-11-08 19:48:32 +0530

    The manacher function is written , only the helper functions are left

--- LongestPalindromicSubstring.cpp
+++ LongestPalindromicSubstring.cpp
@@ -2,11 +2,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 string longestPalindrome(string s){
-    if (s.size()%2==0){
-        // Even length palindrome case
+    // Preprocessing stage :
+    char t[2*s.size()+1];
+    int index=0;
+    for(int i=0;i<2*s.size()+1;i++){
+        if (i%2==0)
+        t[i]='$';
+        else 
+        t[i]=s[index++];
     }
-    else {
-        // Odd length palindrome case 
+    vector<int>p;
+    p.assign(2*s.size()+1,1); // Assigning the minimum value 1 at each location of p
+    int l=1,r=1;
+    // Starting from 1 since on 0th index we know their is no way possible for it to be same 
+    for(int i=1;i<2*s.size()+1;i++){
+        p[i]=max(0,min(r-i,p[l+r-i]));
+        while (i+p[i]<2*s.size()+1 && i-p[i]>=0 && s[i+p[i]]==s[i-p[i]])
+        {
+            p[i]++;
+        }
+        if (i+p[i]>r){
+            l=i+p[i];
+            r=i+p[i] ;       
+        }
     }
 }
 int main(){
