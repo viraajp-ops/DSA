38e4d0012ed6db64518c6c4f9651781de4a8c873
Vaani <vaanipathariya@gmail.com>
2023-11-15 13:03:17 +0530

    https://practice.geeksforgeeks.org/problems/longest-subsequence-such-that-difference-between-adjacents-is-one4724/1

--- LongSub.cpp
+++ LongSub.cpp
@@ -1,4 +1,8 @@
-int ans=0;
+#include<bits/stdc++.h>
+using namespace std;
+int longestSubsequence(int n, int arr[])
+{
+    int ans=1;
     vector<int>sums;
     sums.assign(n,1);
     for(int i=n-2;i>=0;i--){
@@ -12,3 +16,14 @@ int ans=0;
         ans=max(ans,sums[i]);
     }
     return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<< longestSubsequence(n,arr);
+    return 0;
+}
