0712f40258ad7e8b88d19a05b1fb44fd0bcadd8a
Vaani <vaanipathariya@gmail.com>
2024-02-08 05:30:58 +0530

    lca rewrite

--- Lowest_Common_ancester.cpp
+++ Lowest_Common_ancester.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-class node {
+class node{
     public:
     int data;
     node * left ;
@@ -11,28 +11,30 @@ class node {
         right=NULL;
     }
 };
-node * lca(node * root, int n1,int n2){
-    if (root ==NULL)
+node * lowestCommonAncestor(node * root , int n1,int n2){
+    if (root==NULL)
     return NULL;
-    if (root->data==n1 || root->data ==n2)
-    return root;
-    node * LeftLCA=lca(root->left,n1,n2);
-    node * RightLCA=lca(root->right,n1,n2);
-    if (LeftLCA!=NULL && RightLCA==NULL)
-    return LeftLCA;
-    if (LeftLCA==NULL && RightLCA!=NULL)
-    return RightLCA;
-    if (LeftLCA && RightLCA)
-    return root;
-};
+    if (root->data ==n1 || root->data == n2)
+    return root ;
+    node * leftLCA=lowestCommonAncestor(root->left,n1,n2);
+    node * rightLCA=lowestCommonAncestor(root->right, n1,n2);
+    if (leftLCA ==NULL && rightLCA!=NULL)
+    return rightLCA;
+    if (leftLCA !=NULL && rightLCA==NULL)
+    return leftLCA;
+    if (leftLCA && rightLCA)
+    return root ;
+    
+
+}
 int main(){
-    node * root=new node(5);
-    root ->left=new node(2);
-    root->left->left=new node(3);
-    root->left->right=new node(4);
-    int n1,n2;
-    cout<<"Enter the values of n1 and n2";
-    cin>>n1>>n2;
-    cout<<lca(root,n1,n2)->data;
+    node * root = new node(11);
+    root->left=new node(22);
+    root->left->left=new node (44);
+    root->left->right=new node (55);
+    root->right= new node (33);
+    root->right->left=new node (66);
+    root->right->right=new node (77);
+    cout<<lowestCommonAncestor(root , 55 ,77)->data;
     return 0;
 }
